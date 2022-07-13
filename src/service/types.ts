bfa6c41a42f627ef7d59f72ad8999304c7eea95c
frostedpenguin <baltinon2@gmail.com>
2022-07-13 13:07:24 +0300

    small changes

--- src/service/types.ts
+++ src/service/types.ts
@@ -1,10 +1,11 @@
-import { ErmisConfig } from '..';
+import { Config } from '../config';
 
 export type MessageEvent = {
   subject: string;
+  string: string;
   data: any;
 };
 
 export interface IService {
-  publishMessage(messageEvent: MessageEvent, config: ErmisConfig): Promise<void>;
+  publishMessage(messageEvent: MessageEvent, config: Config): Promise<void>;
 }
